4168295b73ee6ce71bd715358b7ad73f4edfb0dd
unknown <peckham4563@gmail.com>
2023-07-23 15:57:39 -0500

    added files

---
+++ MAT-5/SU23_MA5files/SU23_MA5files/SortColumns.m
@@ -0,0 +1,13 @@
+function sortedM = SortColumns(M)
+% Purpose: Sorts each column of the given matrix in ascending order.
+%
+% Inputs:
+%   M = a matrix
+% Outputs:
+%   sortedM = a matrix with columns sorted in ascending order
+%
+% Remember that the above function header is only a template and the names of any input and/or
+% output variables can be changed, if desired.
+
+
+end
